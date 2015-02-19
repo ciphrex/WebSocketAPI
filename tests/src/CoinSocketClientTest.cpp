360085d360478191947d9a487da4a00c26d060b8
Eric Lombrozo <elombrozo@gmail.com>
2015-02-18 19:08:40 -0800

    Put CoinSocketClientTest main body inside try clause.

--- tests/src/CoinSocketClientTest.cpp
+++ tests/src/CoinSocketClientTest.cpp
@@ -19,39 +19,47 @@ int main(int argc, char** argv)
         return 1;
     }
 
-    WebSocket::Client socket("event", "data");
+    try
+    {
+        WebSocket::Client socket("event", "data");
 
-    socket.on("statuschanged", [](const Value& data) {
-        cout << "statuschanged: " << write_string<Value>(data, true) << endl << endl;
-    }).on("txinserted", [](const Value& data) {
-        cout << "txinserted: " << write_string<Value>(data, true) << endl << endl;
-    }).on("txstatuschanged", [](const Value& data) {
-        cout << "txstatuschanged: " << write_string<Value>(data, true) << endl << endl;
-    });
-
-    socket.start(argv[1], [&]() {
-        JsonRpc::Request getvaultinfo("getvaultinfo");
-        socket.send(getvaultinfo, [](const Value& result) {
-            cout << "Command result: " << write_string<Value>(result, true) << endl << endl;
-        }, [](const Value& error) {
-            cout << "Command error: " << write_string<Value>(error, true) << endl << endl; 
+        socket.on("statuschanged", [](const Value& data) {
+            cout << "statuschanged: " << write_string<Value>(data, true) << endl << endl;
+        }).on("txinserted", [](const Value& data) {
+            cout << "txinserted: " << write_string<Value>(data, true) << endl << endl;
+        }).on("txstatuschanged", [](const Value& data) {
+            cout << "txstatuschanged: " << write_string<Value>(data, true) << endl << endl;
         });
 
-        Array params;
-        params.push_back("all");
-        JsonRpc::Request subscribe("subscribe", params); 
-        socket.send(subscribe, [](const Value& result) {
-            cout << "Command result: " << write_string<Value>(result, true) << endl << endl;
-        }, [](const Value& error) {
-            cout << "Command error: " << write_string<Value>(error, true) << endl << endl; 
-        });
-    }, []() {
-        cout << "Connection closed." << endl << endl;
-    }, [](const string& msg) {
-        // cout << "Log: " << msg << endl << endl;
-    }, [](const string& msg) {
-        cout << "Error: " << msg << endl << endl;
-    });
+        socket.start(argv[1], [&]() {
+            JsonRpc::Request getvaultinfo("getvaultinfo");
+            socket.send(getvaultinfo, [](const Value& result) {
+                cout << "Command result: " << write_string<Value>(result, true) << endl << endl;
+            }, [](const Value& error) {
+                cout << "Command error: " << write_string<Value>(error, true) << endl << endl; 
+            });
 
-    return 0;
+            Array params;
+            params.push_back("all");
+            JsonRpc::Request subscribe("subscribe", params); 
+            socket.send(subscribe, [](const Value& result) {
+                cout << "Command result: " << write_string<Value>(result, true) << endl << endl;
+            }, [](const Value& error) {
+                cout << "Command error: " << write_string<Value>(error, true) << endl << endl; 
+            });
+        }, []() {
+            cout << "Connection closed." << endl << endl;
+        }, [](const string& msg) {
+            // cout << "Log: " << msg << endl << endl;
+        }, [](const string& msg) {
+            cout << "Error: " << msg << endl << endl;
+        });
+
+        return 0;
+    }
+    catch (const std::exception& e)
+    {
+        cout << "Exception: " << e.what() << endl;
+        return 2;
+    }
 }
